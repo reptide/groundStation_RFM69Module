729d4593d887cd8d9e752f7aec2e99fd42f81979
Nasheed Ur Rehman <96262883+nasheed-x@users.noreply.github.com>
2023-06-04 01:29:10 +0400

    Updated pinout

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,9 @@
 
 Transceiver *transceiver;
 
-enum state
-{
-    PRELAUNCH,
-    LAUNCH_READY,
-    MOTOR_BURN,
-    BURNOUT,
-    APOGEE,
-    POST_DROGUE,
-    POST_MAIN,
-    END
-};
-
 void setup()
 {
     // Initialize communication
-    Wire.begin();
     Serial.begin(115200);
 
     // Wait until serial console is open, remove if not tethered to computer
@@ -27,14 +14,7 @@ void setup()
         delay(1);
     }
 
-    // gps = new GPS(1000);
     transceiver = new Transceiver(RFM69_CS, RFM69_INT);
-
-    // check_sensors(gps, transceiver)
-        // ? Serial.println("sensors success")
-        // : Serial.println("sensors failed");
-
-    // gps->enable();
     transceiver->enable();
 }
 
